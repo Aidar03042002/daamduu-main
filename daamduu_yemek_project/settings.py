64d739a30422de664cab4cf8bdb14e0275e4cbaf
Aidar Kerimbaev <aker888.kg@gmail.com>
2025-06-12 05:54:35 +0600

    Update settings.py for production: set DEBUG=False and update ALLOWED_HOSTS

--- daamduu_yemek_project/settings.py
+++ daamduu_yemek_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-siq*nbar!x@b414uf-#hi*fwl^%jw-^8#$0h^#gwr3xn(mh*#7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.118.102.243', 'localhost', '127.0.0.1', 'daamduu.duckdns.org']
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'daamduu_yemek_app' / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
